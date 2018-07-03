5dd5623f2487d9b62baa869995f02e21e09f3337
ssakethraj <ssakethraj@gmail.com>
2018-07-03 15:39:53 +0530

    add web push notificatin with service workers

---
+++ src/server.js
@@ -0,0 +1,42 @@
+// Use the web-push library to hide the implementation details of the communication
+// between the application server and the push service.
+// For details, see https://tools.ietf.org/html/draft-ietf-webpush-protocol and
+// https://tools.ietf.org/html/draft-ietf-webpush-encryption.
+var webPush = require('web-push');
+
+if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
+  console.log("You must set the VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY "+
+    "environment variables. You can use the following ones:");
+  console.log(webPush.generateVAPIDKeys());
+  return;
+}
+// Set the keys used for encrypting the push messages.
+webPush.setVapidDetails(
+  'https://serviceworke.rs/',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
+module.exports = function(app, route) {
+  app.get(route + 'vapidPublicKey', function(req, res) {
+    res.send(process.env.VAPID_PUBLIC_KEY);
+  });
+
+  app.post(route + 'register', function(req, res) {
+    // A real world application would store the subscription info.
+    res.sendStatus(201);
+  });
+
+  app.post(route + 'sendNotification', function(req, res) {
+    setTimeout(function() {
+      webPush.sendNotification(req.body.subscription)
+      .then(function() {
+        res.sendStatus(201);
+      })
+      .catch(function(error) {
+        res.sendStatus(500);
+        console.log(error);
+      });
+    }, 10000);
+  });
+};
---
+++ src/tools.js
@@ -0,0 +1,14 @@
+export default function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    var enc = window.btoa(base64);
+    var rawData = window.atob(enc);
+    var outputArray = new Uint8Array(rawData.length);
+    for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
